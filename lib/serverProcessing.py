bca89ad2b6bb50e229b2c8f9749ae930e2408f76
Zack <zack.scholl@gmail.com>
2016-01-24 11:39:21 -0500

    Process data in parallel

--- lib/serverProcessing.py
+++ lib/serverProcessing.py
@@ -1,14 +1,25 @@
 import os
+from multiprocessing import Pool
 
 from lib.compress import *
 
+def saveData(x):
+    d = x[0]
+    datad = x[1]
+    folder = 'data/' + str(int(int(d) / 1000))
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    if not os.path.exists(folder + '/' + str(d) + '.html.lzma'):
+        with open(folder + '/' + str(d) + '.html','w') as f:
+            f.write(datad)
+        os.system('lzma ' + folder + '/' + str(d) + '.html')
+    return True
 
 def processData(dataCompressed):
     data = decompress(dataCompressed)
+    datas = []
     for d in data:
-        folder = 'data/' + str(int(int(d) / 1000))
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        with open(folder + '/' + str(d) + '.html','w') as f:
-            f.write(data[d])
-        os.system('lzma ' + folder + '/' + str(d) + '.html')
+        datas.append((d,data[d]))
+    p = Pool(8)
+    p.map(saveData,datas)
+    p.terminate()
